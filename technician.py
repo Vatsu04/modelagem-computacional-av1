079e1113408556753207fb25f0f99acebf8861fa
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2025-04-19 09:50:46 -0300

    Erro no arquivo technician.py
    Foi concertado o erro, a simulação está funcionando, porém ela está muito rápida

--- technician.py
+++ technician.py
@@ -1,11 +1,11 @@
 from technician_name_generator import generate_technician_names
+import random
 
 
-technicians = generate_technician_names(100)
 
 class Technician:
     technician_id_counter = 0  # Contador global para atribuir IDs únicos aos técnicos
-
+    technicians = generate_technician_names(100)  # Gera uma lista de nomes de técnicos
     def __init__(self, env, technician):
         """
         Inicializa um técnico.
@@ -13,6 +13,7 @@ class Technician:
         :param technician: Os dados do técnico (nome, ID, etc.).
         """
         Technician.technician_id_counter += 1  # Incrementa o contador de IDs
+        self.tech_name = random.choice(Technician.technicians)  # Nome do técnico gerado aleatoriamente
         self.technician_id = Technician.technician_id_counter  # Atribui um ID único ao técnico
         self.env = env  # O ambiente de simulação
         self.technician = technician  # Dados do técnico (use 'technician.tech_name' para acessar o nome)
@@ -27,8 +28,8 @@ class Technician:
         """
         self.busy = True
         self.current_call = call
-        print(f"{self.env.now}: Técnico {self.technician.tech_name} começou a trabalhar no chamado {call.call_id}")
+        print(f"{self.env.now}: Técnico {self.tech_name} começou a trabalhar no chamado {call.call_id}")
         yield self.env.timeout(resolve_time)  # Aguarda o tempo necessário para resolver o chamado
         self.busy = False
         self.current_call = None
-        print(f"{self.env.now}: Técnico {self.technician.tech_name} finalizou o chamado {call.call_id}")
+        print(f"{self.env.now}: Técnico {self.tech_name} finalizou o chamado {call.call_id}")
